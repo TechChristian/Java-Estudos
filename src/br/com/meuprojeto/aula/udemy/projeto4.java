0e57c5278234a5c3b662e29ec39dadb36e1a65d0
Christian Lopes <christianx111.ls@gmail.com>
2025-12-24 00:09:09 -0300

    Desafio utiilizando FOR

---
+++ src/br/com/meuprojeto/aula/udemy/projeto4.java
@@ -0,0 +1,17 @@
+package br.com.meuprojeto.aula.udemy;
+// Faça um programa que imprima na tela apenas os números pares entre 0 e 100.
+
+/* o IF verifica se o número é par, ou seja, se o resto da divisão por 2 é igual a 0.
+    quando a dvisao é 0, ele retorna true e imprime o número,
+    caso contrário, ele retorna false e não imprime o número.
+*/
+
+public class projeto4 {
+    public static void main(String[] args) {
+        for(int i=0; i <=100; i++){
+            if(i % 2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
